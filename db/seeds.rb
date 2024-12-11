2e54ec4b4a429f0b8d7616647dccb5db91cf5023
Thiago Rodrigues Santos <thiago@trsantos.com>
2024-12-11 09:38:15 -0300

    Refresh default seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,31 +7,17 @@ def insert_top_sites(urls)
 end
 
 top_sites = [
-  'https://news.ycombinator.com/rss',
-  'https://lobste.rs/rss',
   'https://feeds.bloomberg.com/technology/news.rss',
   'https://www.theverge.com/rss/full.xml',
   'https://techcrunch.com/feed/',
-  'https://www.nytimes.com/svc/collections/v1/publish/https://www.nytimes.com/section/technology/rss.xml',
   'https://feeds.a.dj.com/rss/RSSWSJD.xml',
+  'https://www.nytimes.com/svc/collections/v1/publish/https://www.nytimes.com/section/technology/rss.xml',
   'https://www.wired.com/feed/rss',
   'https://www.404media.co/rss/',
-  'https://www.theinformation.com/feed',
+  # 'https://www.theinformation.com/feed',
+  'https://www.ft.com/rss/home',
   'http://feeds.arstechnica.com/arstechnica/index',
-  'https://www.cnbc.com/id/19854910/device/rss/rss.html',
-  'https://www.bleepingcomputer.com/feed/',
-  'https://www.androidauthority.com/feed/',
-  'https://9to5mac.com/feed/',
-  'https://9to5google.com/feed/',
-  'https://krebsonsecurity.com/feed/',
-  'https://rss.politico.com/technology.xml',
-  'https://www.windowscentral.com/rss.xml',
-  'https://www.tomshardware.com/feeds.xml',
-  'https://feeds.macrumors.com/MacRumors-All',
-  'https://www.platformer.news/rss/',
-  'https://feeds.bbci.co.uk/news/technology/rss.xml',
-  'https://appleinsider.com/rss/news',
-  'https://www.theregister.com/headlines.atom'
+  'https://www.cnbc.com/id/19854910/device/rss/rss.html'
 ]
 
 insert_top_sites top_sites
