c22a8f47aa0b6b8d2ee7fd6d71e69e823a4f203f
Thiago Rodrigues Santos <thiago@trsantos.com>
2025-03-25 10:53:14 -0300

    Fix Solid Queue configuration
    Hopefully...

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,5 @@ bundle exec bootsnap precompile --gemfile
 bundle exec bootsnap precompile app/ lib/
 bin/rails assets:precompile
 bin/rails db:prepare
+sudo systemctl daemon-reexec
 sudo systemctl restart puma sidekiq
